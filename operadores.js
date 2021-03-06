bd35e16c09ef300a35cee63723b4d795c4eea2da
giova0 <giova0@gmail.com>
2021-03-06 14:20:43 -0500

    Primer archivo

---
+++ operadores.js
@@ -0,0 +1,69 @@
+// operador +
+
+var operandoN1 = 500 +100;
+var operandoN2 = 10000;
+var resultado = operandoN1 + operandoN2;
+console.log(resultado);
+
+// operadores
+
+var operandoN3 = 12;
+var operandoN4 = 2;
+var resultado2 = operandoN3 * operandoN4;
+console.log(resultado2);
+
+
+// operador división
+
+var resultado3 = operandoN3 / operandoN4;
+console.log("el resultado de la división es:", resultado3);
+
+//operador ++  "++ es incremento"
+
+resultado3++;
+console.log("el incremento del resultado3 es:" + resultado3);
+
+// operador =
+
+operandoN1 = operandoN2;
+console.log(operandoN1);
+
+// operador +
+
+operandoN3 += operandoN4;  // suma las variables operandoN3 y operandoN4
+console.log(operandoN3);
+
+// operadores de comparación
+
+var nComparacion1 = 5;
+var nComparacion2 = 5;
+var resultadoComparacion = (nComparacion1 == nComparacion2);
+console.log(resultadoComparacion);
+
+
+// operadores de comparación java tipado débil
+
+var nComparacion11 = 5;
+var nComparacion22 = "5";
+var resultadoComparacion22 = (nComparacion11 == nComparacion22);
+console.log(resultadoComparacion22);
+
+
+// operador de comparación ===
+// el resultado daría falso ya que no son del mismo tipo
+
+var nComparacion11 = 5;
+var nComparacion22 = "5";
+var resultadoComparacion22 = (nComparacion11 === nComparacion22);
+console.log(resultadoComparacion22);  
+
+
+// operador de comparación !==
+// el resultado daría falso ya que no son del mismo tipo
+
+var nComparacion111 = 5;
+var nComparacion222 = 5;
+var resultadoComparacion222 = (nComparacion111 !== nComparacion222);
+console.log(resultadoComparacion222); 
+
+
---
+++ programa1.js
@@ -0,0 +1,25 @@
+// Programa uno En JavaScript
+// Declaración de Variable
+var numero = 365;  
+console.log( numero );
+
+// modificación
+
+numero = 5000;
+console.log( numero );
+
+// Declaración de Constante
+const mensaje = "Bienvenido a ciudad Rio Verde";
+
+//mensaje =  "Bienvenido a ciudad Rio claro";  // no se puede modificar
+console.log( mensaje );
+
+/////
+
+// Características de persona
+const nombre = "Diego";
+const apellidoPaterno = "Pérez";
+const apellidoMaterno = "Dominguez";
+var edad = 15;
+const lugarNacimiento = "México";
+ 
